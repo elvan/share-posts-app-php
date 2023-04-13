8339742f2664eaadfa2d119711e61f89ea925ae3
Elvan Hidayat <elvanhd@gmail.com>
2023-04-14 01:32:39 +0700

    the database class

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,14 +1,20 @@
 <?php
 class Pages extends Controller
 {
+    private $postModel;
+
     public function __construct()
     {
+        $this->postModel = $this->model('Post');
     }
 
     public function index()
     {
+        $posts = $this->postModel->getPosts();
+
         $data = [
-            'title' => 'Welcome'
+            'title' => 'Welcome',
+            'posts' => $posts
         ];
 
         $this->view('pages/index', $data);
