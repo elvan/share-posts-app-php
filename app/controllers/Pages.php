45c6a3e9dd3572f22d73f0d0c69c70726a967a38
Elvan Hidayat <elvanhd@gmail.com>
2023-04-14 00:35:47 +0700

    header and footer includes

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -16,6 +16,10 @@ class Pages extends Controller
 
     public function about()
     {
-        $this->view('pages/about');
+        $data = [
+            'title' => 'About Us'
+        ];
+
+        $this->view('pages/about', $data);
     }
 }
