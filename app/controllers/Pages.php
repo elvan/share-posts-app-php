6755a46846359cd4516d315c63a36baf5a8b277b
Elvan Hidayat <elvanhd@gmail.com>
2023-04-14 02:06:07 +0700

    pages, bootstrap and navbar

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -9,6 +9,7 @@ class Pages extends Controller
     {
         $data = [
             'title' => 'SharePosts',
+            'description' => 'Simple social network built on the MVC PHP framework'
         ];
 
         $this->view('pages/index', $data);
@@ -17,7 +18,8 @@ class Pages extends Controller
     public function about()
     {
         $data = [
-            'title' => 'About Us'
+            'title' => 'About Us',
+            'description' => 'App to share posts with other users'
         ];
 
         $this->view('pages/about', $data);
--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -5,8 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@4.6.2/dist/flatly/bootstrap.min.css" integrity="sha256-GBSk3SnkLpARJ0imUeiqf7XvgjBDjTIZTVUNoJGyGT4=" crossorigin="anonymous">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/style.css">
     <title><?php echo SITENAME; ?></title>
 </head>
 
 <body>
+    <?php require APPROOT . '/views/inc/navbar.php'; ?>
+    <div class="container">
