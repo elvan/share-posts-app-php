bcde2fcedc8df1eaee5d960e622a1de36224ced1
Elvan Hidayat <elvanhd@gmail.com>
2023-04-14 00:28:58 +0700

    config files and autoloader

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,5 +1,8 @@
 <?php
-// Load Libraries
-require_once 'libraries/core.php';
-require_once 'libraries/controller.php';
-require_once 'libraries/database.php';
+// Load Config
+require_once 'config/config.php';
+
+// Autoload Core Libraries
+spl_autoload_register(function ($className) {
+    require_once 'libraries/' . $className . '.php';
+});
---
+++ app/config/config.php
@@ -0,0 +1,7 @@
+<?php
+// App Root
+define('APPROOT', dirname(dirname(__FILE__)));
+// URL Root
+define('URLROOT', 'http://share-posts.example.local');
+// Site Name
+define('SITENAME', 'SharePosts');
