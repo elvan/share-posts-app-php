f02c787c323e548f5e4c43d163b17ab4e0f2661b
Elvan Hidayat <elvanhd@gmail.com>
2023-04-14 02:30:38 +0700

    register and login form views

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -27,4 +27,23 @@ class Users extends Controller
             $this->view('users/register', $data);
         }
     }
+
+    public function login()
+    {
+        // Check for POST
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            // Process form
+        } else {
+            // Init data
+            $data = [
+                'email' => '',
+                'password' => '',
+                'email_err' => '',
+                'password_err' => '',
+            ];
+
+            // Load view
+            $this->view('users/login', $data);
+        }
+    }
 }
