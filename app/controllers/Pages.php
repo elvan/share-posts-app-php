4a6630482bdb863070987e3b7007e26848a3189e
Elvan Hidayat <elvanhd@gmail.com>
2023-04-14 01:44:07 +0700

    initial app setup

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,20 +1,14 @@
 <?php
 class Pages extends Controller
 {
-    private $postModel;
-
     public function __construct()
     {
-        $this->postModel = $this->model('Post');
     }
 
     public function index()
     {
-        $posts = $this->postModel->getPosts();
-
         $data = [
-            'title' => 'Welcome',
-            'posts' => $posts
+            'title' => 'SharePosts',
         ];
 
         $this->view('pages/index', $data);
