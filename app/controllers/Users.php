0a424ba6fe595c9174747c8d604c5fd29268bb75
Elvan Hidayat <elvanhd@gmail.com>
2023-04-14 03:25:18 +0700

    user registration

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -63,7 +63,16 @@ class Users extends Controller
             // Make sure errors are empty
             if (empty($data['email_err']) && empty($data['name_err']) && empty($data['password_err']) && empty($data['confirm_password_err'])) {
                 // Validated
-                die('SUCCESS');
+
+                // Hash Password
+                $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
+
+                // Register User
+                if ($this->userModel->register($data)) {
+                    redirect('users/login');
+                } else {
+                    die('Something went wrong');
+                }
             } else {
                 // Load view with errors
                 $this->view('users/register', $data);
--- app/models/User.php
+++ app/models/User.php
@@ -8,10 +8,28 @@ class User
         $this->db = new Database;
     }
 
+    // Regsiter user
+    public function register($data)
+    {
+        $this->db->query('INSERT INTO users (name, email, password) VALUES(:name, :email, :password)');
+        // Bind values
+        $this->db->bind(':name', $data['name']);
+        $this->db->bind(':email', $data['email']);
+        $this->db->bind(':password', $data['password']);
+
+        // Execute
+        if ($this->db->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     // Find user by email
     public function findUserByEmail($email)
     {
         $this->db->query('SELECT * FROM users WHERE email = :email');
+        // Bind value
         $this->db->bind(':email', $email);
 
         $row = $this->db->single();
