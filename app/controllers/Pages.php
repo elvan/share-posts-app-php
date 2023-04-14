ebfadd3f6305b59cb2b21484c3e46f1f96468181
Elvan Hidayat <elvanhd@gmail.com>
2023-04-14 14:56:06 +0700

    add post form

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -7,9 +7,13 @@ class Pages extends Controller
 
     public function index()
     {
+        if (isLoggedIn()) {
+            redirect('posts');
+        }
+
         $data = [
             'title' => 'SharePosts',
-            'description' => 'Simple social network built on the MVC PHP framework'
+            'description' => 'Simple social network built on the TraversyMVC PHP framework'
         ];
 
         $this->view('pages/index', $data);
--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -23,4 +23,14 @@ class Posts extends Controller
 
         $this->view('posts/index', $data);
     }
+
+    public function add()
+    {
+        $data = [
+            'title' => '',
+            'body' => ''
+        ];
+
+        $this->view('posts/add', $data);
+    }
 }
