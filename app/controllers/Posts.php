2bc176bddc55a04703b15a262e7b7dbb280b0746
Elvan Hidayat <elvanhd@gmail.com>
2023-04-14 14:47:08 +0700

    post model and display

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -1,16 +1,25 @@
 <?php
 class Posts extends Controller
 {
+    private $postModel;
+
     public function __construct()
     {
         if (!isLoggedIn()) {
             redirect('users/login');
         }
+
+        $this->postModel = $this->model('Post');
     }
 
     public function index()
     {
-        $data = [];
+        // Get posts
+        $posts = $this->postModel->getPosts();
+
+        $data = [
+            'posts' => $posts
+        ];
 
         $this->view('posts/index', $data);
     }
--- app/views/posts/index.php
+++ app/views/posts/index.php
@@ -1,5 +1,5 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
-<div class="row">
+<div class="row mb-3">
     <div class="col-md-6">
         <h1>Posts</h1>
     </div>
@@ -9,4 +9,14 @@
         </a>
     </div>
 </div>
+<?php foreach ($data['posts'] as $post) : ?>
+    <div class="card card-body mb-3">
+        <h4 class="card-title"><?php echo $post->title; ?></h4>
+        <div class="bg-light p-2 mb-3">
+            Written by <?php echo $post->name; ?> on <?php echo $post->postCreated; ?>
+        </div>
+        <p class="card-text"><?php echo $post->body; ?></p>
+        <a href="<?php echo URLROOT; ?>/posts/show/<?php echo $post->postId; ?>" class="btn btn-dark">More</a>
+    </div>
+<?php endforeach; ?>
 <?php require APPROOT . '/views/inc/footer.php'; ?>
