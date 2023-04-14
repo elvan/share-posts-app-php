7487d65a4b51b124e9de3b23b558ca14b595b75d
Elvan Hidayat <elvanhd@gmail.com>
2023-04-14 16:31:14 +0700

    editing posts

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -71,6 +71,61 @@ class Posts extends Controller
         }
     }
 
+    public function edit($id)
+    {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            // Sanitize POST array
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+            $data = [
+                'id' => $id,
+                'title' => trim($_POST['title']),
+                'body' => trim($_POST['body']),
+                'user_id' => $_SESSION['user_id'],
+                'title_err' => '',
+                'body_err' => ''
+            ];
+
+            // Validate data
+            if (empty($data['title'])) {
+                $data['title_err'] = 'Please enter title';
+            }
+            if (empty($data['body'])) {
+                $data['body_err'] = 'Please enter body text';
+            }
+
+            // Make sure no errors
+            if (empty($data['title_err']) && empty($data['body_err'])) {
+                // Validated
+                if ($this->postModel->updatePost($data)) {
+                    flash('post_message', 'Post Updated');
+                    redirect('posts');
+                } else {
+                    die('Something went wrong');
+                }
+            } else {
+                // Load view with errors
+                $this->view('posts/edit', $data);
+            }
+        } else {
+            // Get existing post from model
+            $post = $this->postModel->getPostById($id);
+
+            // Check for owner
+            if ($post->user_id != $_SESSION['user_id']) {
+                redirect('posts');
+            }
+
+            $data = [
+                'id' => $id,
+                'title' => $post->title,
+                'body' => $post->body
+            ];
+
+            $this->view('posts/edit', $data);
+        }
+    }
+
     public function show($id)
     {
         $post = $this->postModel->getPostById($id);
