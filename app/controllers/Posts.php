242d127b3859b5bf372967a4db0068755525722b
Elvan Hidayat <elvanhd@gmail.com>
2023-04-14 15:10:10 +0700

    inserting posts

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -26,11 +26,46 @@ class Posts extends Controller
 
     public function add()
     {
-        $data = [
-            'title' => '',
-            'body' => ''
-        ];
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            // Sanitize POST array
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
 
-        $this->view('posts/add', $data);
+            $data = [
+                'title' => trim($_POST['title']),
+                'body' => trim($_POST['body']),
+                'user_id' => $_SESSION['user_id'],
+                'title_err' => '',
+                'body_err' => ''
+            ];
+
+            // Validate data
+            if (empty($data['title'])) {
+                $data['title_err'] = 'Please enter title';
+            }
+            if (empty($data['body'])) {
+                $data['body_err'] = 'Please enter body text';
+            }
+
+            // Make sure no errors
+            if (empty($data['title_err']) && empty($data['body_err'])) {
+                // Validated
+                if ($this->postModel->addPost($data)) {
+                    flash('post_message', 'Post Added');
+                    redirect('posts');
+                } else {
+                    die('Something went wrong');
+                }
+            } else {
+                // Load view with errors
+                $this->view('posts/add', $data);
+            }
+        } else {
+            $data = [
+                'title' => '',
+                'body' => ''
+            ];
+
+            $this->view('posts/add', $data);
+        }
     }
 }
