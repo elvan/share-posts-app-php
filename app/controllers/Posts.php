238ef4bcf65151e4f05a20f5191db23c649f93ec
Elvan Hidayat <elvanhd@gmail.com>
2023-04-14 14:05:10 +0700

    posts access control

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -1,6 +1,13 @@
 <?php
 class Posts extends Controller
 {
+    public function __construct()
+    {
+        if (!isLoggedIn()) {
+            redirect('users/login');
+        }
+    }
+
     public function index()
     {
         $data = [];
